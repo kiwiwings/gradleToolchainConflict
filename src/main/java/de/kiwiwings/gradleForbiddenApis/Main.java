d2d0b4879850416f9d328fd7efb19e38628e447a
kiwiwings <andreas.beeker@gmx.de>
2022-02-28 22:59:42 +0100

    add for bug report

---
+++ src/main/java/de/kiwiwings/gradleForbiddenApis/Main.java
@@ -0,0 +1,7 @@
+package de.kiwiwings.gradleForbiddenApis;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Gradle, why don't you compile me?!");
+    }
+}
